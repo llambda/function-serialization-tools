9a4a6707827d263cb3374d1d75e8be7b30b637b2
Grant Miner <ggcode1@users.noreply.github.com>
2017-02-09 21:59:12 -0600

    Convert to tape for tests.

--- test.js
+++ test.js
@@ -1,18 +1,38 @@
-var assert = require('assert');
-var fntools = require('./index.js');
+const test = require('tape');
+const fntools = require('./index.js');
 
-function mult(x, y) {
-	return x*y;
-}
+test('string to function works', function (t) {
+	t.plan(1);
 
-var funAsString = mult.toString();
+	function mult(x, y) {
+		return x*y;
+	}
 
-var y = fntools.s2f(funAsString);
+	const funAsString = mult.toString();
+	const y = fntools.s2f(funAsString);
 
-assert.equal(y(3,7), 3*7);
-assert.equal(3*7, eval( fntools.apply2s(y, [3, 7]) ));
+	t.equal(y(3,7), 3*7);
+});
 
-assert.throws(
-	function () {
-		fntools.s2f('asdf')
-	}, TypeError);
+test('apply to string works', function (t) {
+	t.plan(1);
+
+	function mult(x, y) {
+		return x*y;
+	}
+
+	const funAsString = mult.toString();
+	const y = fntools.s2f(funAsString);
+
+	t.equal(3*7, eval( fntools.apply2s(y, [3, 7]) ));
+});
+
+test('invalid function string throws', function (t) {
+
+	t.plan(1);
+	
+	t.throws(
+		function () {
+			fntools.s2f('asdf')
+		}, TypeError);		
+})
